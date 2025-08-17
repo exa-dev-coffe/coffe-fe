435211a8e0fd89767eb3a0ccc856bdc9bb16a54c
Moh Eka Syafrino Nazhifan <bloodsuker18@gmail.com>
2025-08-17 21:37:10 +0700

    feat: done slicing activate wallet part 2

--- src/model/wallet.ts
+++ src/model/wallet.ts
@@ -1,4 +1,5 @@
 import type {BaseResponse} from "./index.ts";
+import {z} from "zod";
 
 export type Wallet = {
     is_active: boolean;
@@ -6,3 +7,16 @@ export type Wallet = {
 };
 
 export type ResponseCheckWallet = BaseResponse<Wallet>
+
+export const WalletSchema = z.object({
+    pin: z.string().min(6, "PIN  must be at least 6 characters").max(6, "PIN must be exactly 6 characters"),
+    confirmPin: z.string().min(6, "Confirm PIN must be at least 6 characters").max(6, "Confirm PIN must be exactly 6 characters"),
+}).refine(
+    (data) => data.pin === data.confirmPin,
+    {
+        message: "PIN and Confirm PIN must match",
+        path: ["confirmPin"],
+    }
+)
+
+export  type BodySetPin = z.infer<typeof WalletSchema>;
--- src/pages/client/ActivateWallet.tsx
+++ src/pages/client/ActivateWallet.tsx
@@ -1,11 +1,47 @@
 import Modal from "../../component/ui/Modal.tsx";
 import Icon from "../../assets/images/icon.png";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {IoWalletOutline} from "react-icons/io5";
+import InputIcon from "../../component/ui/form/InputIcon.tsx";
+import {GoKey} from "react-icons/go";
+import useWallet from "../../hook/useWallet.tsx";
+import {useNavigate} from "react-router";
 
 const ActivateWalletPage = () => {
 
     const [open, setOpen] = useState(true);
+    const [form, setForm] = useState({
+        pin: '',
+        confirmPin: '',
+    });
+    const navigate = useNavigate();
+    const {checkWallet} = useWallet()
+
+    useEffect(() => {
+        const fetchWallet = async () => {
+            const res = await checkWallet();
+            if (res && res.data && res.data.is_active) {
+                navigate('/my-wallet');
+            }
+        };
+        fetchWallet();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        const validPin = /^[0-9]*$/; // Regex to allow only numbers
+        if (name === 'pin' || name === 'confirmPin') {
+            if (!validPin.test(value)) {
+                return; // Prevent non-numeric input
+            }
+        }
+
+        setForm(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
 
     return (
         <section className="container mx-auto my-10">
@@ -66,6 +102,36 @@ const ActivateWalletPage = () => {
                 <h3 className={'text-3xl text-center font-bold mb-4'}>
                     Setup Your Wallet
                 </h3>
+                <form className={'w-1/3 space-y-10 my-14  mx-auto'} onSubmit={e => e.preventDefault()}>
+                    <InputIcon
+                        error={''}
+                        disabled={false}
+                        name={'pin'}
+                        value={form.pin}
+                        onChange={handleChange}
+                        label="PIN"
+                        placeholder="Type your pin"
+                        icon={<GoKey/>}
+                        type="text"
+                        required={true}
+                    />
+                    <InputIcon
+                        error={''}
+                        disabled={false}
+                        name={'confirmPin'}
+                        value={form.confirmPin}
+                        onChange={handleChange}
+                        label="Retype PIN"
+                        placeholder="Retype your pin"
+                        icon={<GoKey/>}
+                        type="text"
+                        required={true}
+                    />
+                    <button className={'btn-primary px-10 py-3 mt-5 text-white rounded-xl w-full'} type="submit"
+                    >
+                        Activate
+                    </button>
+                </form>
             </div>
         </section>
     )
