f402bcb42b6f9c97fdc7a4e63853e4dd4e227369
Moh Eka Syafrino Nazhifan <bloodsuker18@gmail.com>
2025-10-18 05:21:51 +0700

    feat: refactor CardTransaction component to use camelCase for props and improve status handling

--- src/component/ui/card/CardTransaction.tsx
+++ src/component/ui/card/CardTransaction.tsx
@@ -3,48 +3,42 @@ import {Link} from "react-router";
 
 interface CardTransactionProps {
     id: number;
-    status: number;
-    status_label: string;
-    order_for: string;
-    order_by: string;
-    user_id: number;
+    orderStatus: number;
     maxItems: number;
-    order_table: string;
-    table_id: number;
-    total_price: number;
-    created_at: string;
+    orderTable: string;
+    totalPrice: number;
+    createdAt: string;
     details: {
         id: number;
-        menu_id: number;
-        menu_name: string;
+        menuName: string;
         photo: string;
-        qty: number;
         price: number;
-        total_price: number;
-        notes: string;
-        rating: number;
     }[];
 }
 
 const CardTransaction: React.FC<CardTransactionProps> = ({
                                                              details, maxItems,
                                                              id,
-                                                             order_table,
-                                                             status_label,
-                                                             status,
-                                                             total_price,
-                                                             created_at
+                                                             orderTable,
+                                                             orderStatus,
+                                                             totalPrice,
+                                                             createdAt
                                                          }) => {
 
+    const statusLabel = [
+        'Order Confirmed',
+        'Delivering Order',
+        'Order Completed'
+    ]
 
     return (
         <div data-aos={'fade-up'} className={'mt-10 bg-white p-8 rounded-2xl '}>
             <div className={'flex justify-between'}>
                 <h4 className={'text-xl font-bold'}>
-                    {order_table}
+                    {orderTable}
                 </h4>
                 <p>
-                    {formatDateTimeShortString(created_at)}
+                    {formatDateTimeShortString(createdAt)}
                 </p>
             </div>
             <div className={'flex justify-between my-7'}>
@@ -52,11 +46,11 @@ const CardTransaction: React.FC<CardTransactionProps> = ({
                     {
                         details.slice(0, maxItems).map((detail, detailIndex) => (
                             <div key={detailIndex}>
-                                <img src={`${import.meta.env.VITE_APP_IMAGE_URL}/${detail.photo}`}
+                                <img src={`${detail.photo}`}
                                      className={'xl:w-56 sm:w-44 xl:h-56 sm:h-44 w-28 h-28 object-cover rounded-xl'}
-                                     alt={detail.menu_name}/>
+                                     alt={detail.menuName}/>
                                 <h6 className={'text-sm sm:text-xl mt-4   w-28  xl:w-56 sm:w-44 truncate'}>
-                                    {detail.menu_name}
+                                    {detail.menuName}
                                 </h6>
                             </div>
                         ))
@@ -65,7 +59,7 @@ const CardTransaction: React.FC<CardTransactionProps> = ({
                 <div className={'text-end '}>
 
                     <h5 className={'font-bold text-base sm:text-2xl mt-7 mb-3'}>
-                        {formatCurrency(total_price)}
+                        {formatCurrency(totalPrice)}
                     </h5>
                     <p className={' text-sm sm:text-xl mt-7 mb-3'}>
                         {details.length} Menu
@@ -73,9 +67,9 @@ const CardTransaction: React.FC<CardTransactionProps> = ({
                 </div>
             </div>
             <div className={'flex justify-between items-center border-t border-gray-300 '}>
-                <h5 className={`text-sm sm:text-xl font-bold mt-7 mb-3 ${status === 1 ? `text-[#F9A825]` : status === 3 ? `text-[#47DC53]` : status === 2 ? `text-[#DDE232]` : ``} `}>
+                <h5 className={`text-sm sm:text-xl font-bold mt-7 mb-3 ${orderStatus === 0 ? `text-[#F9A825]` : orderStatus === 2 ? `text-[#47DC53]` : orderStatus === 1 ? `text-[#DDE232]` : ``} `}>
                     {
-                        status_label
+                        statusLabel[orderStatus]
                     }
                 </h5>
                 <Link to={'/my-transaction/' + id}
